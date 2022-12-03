aae43e171f51391658419df94f1c4619aadbab0e
Niky <nick.vusko@gmail.com>
2022-12-03 13:25:35 +0100

    day_3 update

--- day_3.py
+++ day_3.py
@@ -1,5 +1,6 @@
 import string
 
+
 def parse_input(fh: str) -> list:
     with open(f"{fh}.txt") as f:
         lines = [x.strip("\n") for x in f.readlines()]
@@ -32,7 +33,26 @@ def day_3_1(file: str):
     return total_value
 
 
-print(day_3_1("input"))
+def day_3_2(file: str):
+    letters_lower = {key: ord(f"{key}")-96 for key in string.ascii_lowercase}
+    letters_upper = {key: ord(f"{key}")-38 for key in string.ascii_uppercase}
+    all_letters = {**letters_lower, **letters_upper}
+    bags = parse_input(file)
+    badges = []
+    for i in range(0, len(bags), 3):
+        group = bags[i:i+3]
+        common = dict.fromkeys(all_letters.keys(), 0)
+        for bag in group:
+            letter_tracker = []
+            for letter in bag:
+                if letter not in letter_tracker:
+                    letter_tracker.append(letter)
+                    common[letter] += 1
+        badges.append(max(common, key=common.get))
+    total_value = 0
+    for found in badges:
+        total_value += all_letters[found]
+    return total_value
 
 
-
+print(day_3_2("input"))
